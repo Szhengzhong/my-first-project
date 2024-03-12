d9aac46c6696183c82d2f3789f558feedcf4c2d7
Shengqiang Cui <shengqiang@encompass8.cn>
2024-03-12 12:59:09 +0800

    Task 2003806: Cone: Screenshot shows nothing under screen name, because they are missing.

--- kiosk/src/encompass.ts
+++ kiosk/src/encompass.ts
@@ -8,7 +8,7 @@ import credentials from "./credentials";
 
 export default class Encompass {
     static version(): string {
-        const version = "22.7.1";
+        const version = "22.7.2";
         return version;
     }
 
